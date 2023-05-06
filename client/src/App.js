b9d941079157df4607c06969d67bd43feb44da8f
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 02:36:15 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
+/*third party */
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+/*Internal Source */
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import Genres from './pages/Genres';
+import Admin from './pages/Admin';
+
+
+/* Style */
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="container" >
       <div className="row">
         <h1 className="mt-3">Go React Movie Project</h1>
         <hr className="mb-3" />
       </div>
       <div className="row">
-        <div className="col-2">
-          <span>Menu</span>
+        <div className="col-2 mb-3">
+          <Menu/>
         </div>
         <div className="col-10">
-          <span>Content</span>
+            <Routes>
+              <Route path='/' element={<Home /> } />
+              <Route path='/movies' element={<Movies /> } />
+              <Route path='/genres' element={<Genres /> } />
+              <Route path='/admin' element={<Admin /> } />
+            </Routes>
         </div>
       </div>
     </div>
-    
+    </Router>
   );
 }
 
