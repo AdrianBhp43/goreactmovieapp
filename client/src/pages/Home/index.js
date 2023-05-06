798efcae71d4cf048f6d254199b4bd7c4f3c4f7c
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 02:48:01 +0700

    commit

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import MovieList from '../../components/MovieList'
 
 const index = () => {
     return (
-        <div>
-            index
-        </div>
+        <>
+            <MovieList />
+        </>
     )
 }
 
