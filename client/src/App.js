4143f939f5dea9c1e4135101d73cd52f68770fd4
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 01:32:11 +0700

    commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container" >
+      <div className="row">
+        <h1 className="mt-3">Go React Movie Project</h1>
+        <hr className="mb-3" />
+      </div>
+      <div className="row">
+        <div className="col-2">
+          <span>Menu</span>
+        </div>
+        <div className="col-10">
+          <span>Content</span>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
