aadd12a8c94c24bb30d7621b3b081e0998f9d4a1
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 12:19:42 +0700

    commit

---
+++ client/src/components/MovieList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+    //const id = 1;
+    useEffect(() => {
+        setMovies([
+            { id: 1, tittle: 'Spongebob Squarepants', runtime: 145 },
+            { id: 1, tittle: 'Ogry', runtime: 145 },
+            { id: 1, tittle: 'Chalk Zone', runtime: 145 },
+        ]);
+    }, []);
+
+
+        return (
+            <div className="row">
+                    <div className="col-sm-4 mb-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">
+                                    With supporting text below as a natural lead-in to additional content.
+                                </p>
+                                <a href="#" className="btn btn-primary">
+                                    Go somewhere
+                                </a>
+                            </div>
+            </div>
+        )
+    }
+
+export default MovieList;
