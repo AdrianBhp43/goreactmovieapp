94d568859394b491a66e6d7766780fd6281c1b98
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 12:24:49 +0700

    commit

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -12,21 +12,25 @@ const MovieList = () => {
     }, []);
 
 
-        return (
-            <div className="row">
-                    <div className="col-sm-4 mb-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">
-                                    With supporting text below as a natural lead-in to additional content.
-                                </p>
-                                <a href="#" className="btn btn-primary">
-                                    Go somewhere
-                                </a>
-                            </div>
+    return (
+        <div className="row">
+            {movies.map((movie) =>  (
+            <div className="col-sm-4 mb-2" key={index}>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{movie.title}</h5>
+                        <p className="card-text">
+                            With supporting text below as a natural lead-in to additional content.
+                        </p>
+                        <a href="#" className="btn btn-primary">
+                            Go somewhere
+                        </a>
+                    </div>
+                </div>
             </div>
-        )
-    }
+                ))}
+        </div>
+    );
+};
 
 export default MovieList;
