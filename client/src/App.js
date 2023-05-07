1e8ff9738acca7b66b7835526a5b88da29e58280
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-07 14:22:18 +0700

    commit lagi

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Menu from './components/Menu';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
-import ShowMovie from './pages/Movies/Show'
+import ShowMovie from './pages/Movies/Show';
 //import MovieDetail from './components/movies/MovieDetail';
 import Genres from './pages/Genres';
 import Admin from './pages/Admin';
