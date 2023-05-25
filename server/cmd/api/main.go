c9696d9e85cb3235837eb35649b5b1448bb2c739
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-05-25 13:17:44 +0700

    commit lagi

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
@@ -21,6 +22,11 @@ type AppStatus struct {
 	Version     string `json:"version"`
 }
 
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
 	var cfg config
 
@@ -28,26 +34,44 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "application environment (development|production)")
 	flag.Parse()
 
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
 	fmt.Println("Server is running...")
 
-	http.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus{
-			Status:      "Online",
-			Environment: cfg.env,
-			Version:     version,
-		}
+	// http.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
+	// 	currentStatus := AppStatus{
+	// 		Status:      "Online",
+	// 		Environment: cfg.env,
+	// 		Version:     version,
+	// 	}
 
-		res, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			fmt.Println(err)
-		}
+	// 	res, err := json.MarshalIndent(currentStatus, "", "\t")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		rw.Write(res)
-	})
+	// 	rw.Header().Set("Content-Type", "application/json")
+	// 	rw.WriteHeader(http.StatusOK)
+	// 	rw.Write(res)
+	// })
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	//err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	serve := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("Starting server on port", cfg.port)
+
+	err := serve.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 
---
+++ server/cmd/api/movieHandler.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(rw http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
+	}
+
+	app.logger.Println("the id is: ", id)
+}
